efa363745aad601727bb7dcbbd7d419adec841c5
laptop <laptop@scy.com>
2020-01-18 20:18:53 +0800

    feat add 天池

---
+++ source/tianchi.py
@@ -0,0 +1,44 @@
+from requests import request
+
+from source.tool import data2md
+
+PLATFORM_NAME = '天池'
+
+
+def get_data():
+    url = 'https://tianchi.aliyun.com/competition/proxy/api/competition/api/race/listBrief?pageNum=1&pageSize=10&type=1&userId=-1'
+
+    response = request(method='GET', url=url)
+    content = response.json()
+    competitions = content['data']['list']
+
+    data = {'name':competition PLATFORM_NAME}
+    cps = []
+    for competition in competitions:
+        # 必须字段
+        name = competition['raceName']
+        url = 'https://tianchi.aliyun.com/competition/entrance/{}/introduction'.format(
+            competition['raceId'])
+        description = competition['brief']
+        deadline = competition['currentSeasonEnd']
+        reward = competition['currencySymbol'] + str(competition['bonus'])
+
+        cp = {
+            'name': name,
+            'url': url,
+            'description': description,
+            'deadline': deadline,
+            'reward': reward
+        }
+
+        cps.append(cp)
+    data['competitions'] = cps
+
+    return data
+
+
+def update():
+    data = get_data()
+    md_text = data2md(data)
+
+    return md_text
--- update.py
+++ update.py
@@ -1,9 +1,10 @@
-from source import kaggle, turingtopia
+from source import kaggle, turingtopia, tianchi
 
-text = ''
+text = '一站式显示各大数据竞赛平台正在进行的比赛，每天 21:00 UTC（北京时间早上5点）更新。  \n'
 
 text += kaggle.update()
 text += turingtopia.update()
+text += tianchi.update()
 
 # 写入到文件
 with open('README.md', 'w') as f:
