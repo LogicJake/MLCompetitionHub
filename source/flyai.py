bc61ac00f39515108e7a392f735563da1d66db30
laptop <laptop@scy.com>
2020-02-20 13:56:31 +0800

    feat add flyai

---
+++ source/flyai.py
@@ -0,0 +1,76 @@
+from datetime import datetime, timedelta
+
+import requests
+from lxml import etree
+
+from .utils import STANDARD_TIME_FORMAT, MAX_INTERVAL_DAY
+
+PLATFORM_NAME = 'FlyAi'
+
+
+def get_data():
+    url = 'https://www.flyai.com/'
+
+    response = requests.get(url=url)
+    html = response.text
+    html = etree.HTML(html)
+    css_list = html.cssselect('div.pro_box a.project.challage_itemInfo')
+
+    data = {'name': PLATFORM_NAME}
+    cps = []
+    for info in css_list:
+        try:
+            info.cssselect('p.noReg')[0].text.strip()
+        except Exception:
+            continue
+
+        name = info.cssselect('div.name h2')[0].text.strip()
+        url = info.attrib['href'].strip()
+        description = info.cssselect('div.describe')[0].text.strip()
+
+        response = requests.get(url=url)
+        html = response.text
+        html = etree.HTML(html)
+        time = html.cssselect(
+            'div.contest_info > div:nth-child(3) > p')[0].text.strip()
+
+        if '-' in time:
+            start_time = time.split('-')[0].strip()
+            start_time = start_time.split('：')[1].strip()
+
+            deadline = time.split('-')[1].strip()
+            FORMAT = "%Y.%m.%d %H:%M:%S"
+            deadline = datetime.strptime(deadline, FORMAT)
+            deadline = deadline.strftime(STANDARD_TIME_FORMAT)
+
+            start_time = datetime.strptime(start_time, FORMAT)
+            now_time = datetime.utcnow() + timedelta(hours=8)
+            interval = now_time - start_time
+            if interval.days < MAX_INTERVAL_DAY:
+                new_flag = True
+            else:
+                new_flag = False
+        else:
+            deadline = '无具体截止日期'
+            start_time = None
+            new_flag = False
+
+        try:
+            reward = info.cssselect('div.bonus.flex div')[0].text.strip()
+        except Exception:
+            reward = info.cssselect('div.bonus.flex p')[0].text.strip()
+
+        cp = {
+            'name': name,
+            'url': url,
+            'description': description,
+            'deadline': deadline,
+            'reward': reward,
+            'start_time': start_time,
+            'new_flag': new_flag
+        }
+        cps.append(cp)
+
+    data['competitions'] = cps
+
+    return data
--- update.py
+++ update.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 from jinja2 import Environment, PackageLoader
 
-from source import dcjingsai, kaggle, tianchi, turingtopia, biendata, datafountain, kesci, dianshi, yanxishe, huaweicloud
+from source import dcjingsai, kaggle, tianchi, turingtopia, biendata, datafountain, kesci, dianshi, yanxishe, huaweicloud, flyai
 
 # 获取数据
 datas = []
@@ -55,6 +55,11 @@ except Exception:
 
 try:
     datas.append(huaweicloud.get_data())
+except Exception:
+    traceback.print_exc()
+
+try:
+    datas.append(flyai.get_data())
 except Exception:
     traceback.print_exc()
 
