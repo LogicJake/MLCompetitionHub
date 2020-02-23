27bfba15df949d3e42886bf9a57e1b0a31304cc8
laptop <laptop@scy.com>
2020-02-23 10:18:40 +0800

    url 增量式更新

--- actions/zlink.py
+++ actions/zlink.py
@@ -1,10 +1,20 @@
 def generate(datas):
+    # 增量式更新
+    try:
+        with open('urls.txt', 'r') as f:
+            urls = f.readlines()
+    except Exception:
+        urls = []
 
-    urls = []
+    urls = [url.strip() for url in urls]
+
     for data in datas:
         for c in data['competitions']:
-            url = c['url'] + '\n'
-            urls.append(url)
+            url = c['url']
+            if url not in urls:
+                urls.append(url)
+
+    urls = [url + '\n' for url in urls]
 
     with open('urls.txt', 'w') as f:
         f.writelines(urls)
