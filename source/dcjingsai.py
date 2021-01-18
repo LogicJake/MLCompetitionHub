c643048fed87b9d92ea472fde649619ffaae6fa9
scy_lab <835410808@qq.com>
2021-01-18 20:36:29 +0800

    update dcjingsai

--- source/dcjingsai.py
+++ source/dcjingsai.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-from urllib import parse
 
 import requests
 
@@ -7,35 +6,27 @@ PLATFORM_NAME = 'DC竞赛'
 
 
 def get_data():
-    url = 'https://www.dcjingsai.com/common/getNewCmptList.json'
-    data = {
-        'page': 1,
-        'pageSize': 10,
-        'type': 'SUBMITRESULT',
-        'state': 'active'
-    }
+    url = 'https://js.dclab.run/v2/api/common/cmpt/getCmptList.json?cmptName=&order=desc&orderType=&page=1&pageSize=10&state=active&type=%E7%AE%97%E6%B3%95%E8%B5%9B'
 
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    response = requests.post(url=url,
-                             data=parse.urlencode(data),
-                             headers=headers)
+    response = requests.get(url=url)
     content = response.json()
-    competitions = content['data']['cmptList']['list']
+    competitions = content['data']['map']['list']
 
     data = {'name': PLATFORM_NAME}
     cps = []
     for competition in competitions:
-        if competition['rewardType'] != '奖金':
+        if competition['reward_type'] != '奖金':
             continue
         # 必须字段
         name = competition['name']
-        url = 'https://www.dcjingsai.com/common/cmpt/{}_竞赛信息.html'.format(name)
+        id = competition['id']
+        url = 'https://js.dclab.run/v2/cmptDetail.html?id={}'.format(id)
         description = competition['introduction']
 
-        deadline = datetime.utcfromtimestamp(int(competition['endTime'] /
-                                                 1000))
+        deadline = datetime.utcfromtimestamp(
+            int(competition['end_time'] / 1000))
         start_time = datetime.utcfromtimestamp(
-            int(competition['startTime'] / 1000))
+            int(competition['start_time'] / 1000))
 
         deadline = deadline + timedelta(hours=8)
         start_time = start_time + timedelta(hours=8)
