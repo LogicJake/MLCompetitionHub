b27eebb00f0caca1a2f48927a8891ab9877da9c3
labserver22 <835410808@qq.com>
2021-12-28 16:28:08 +0800

    update dc jingsai

--- source/dcjingsai.py
+++ source/dcjingsai.py
@@ -5,8 +5,22 @@ import requests
 PLATFORM_NAME = 'DC竞赛'
 
 
+def parse_competition(competition):
+    if competition['rewardType'] != '奖金':
+        return
+    # 必须字段
+    name = competition['cmptName']
+    id = competition['id']
+    url = 'https://challenge.datacastle.cn/v3/cmptDetail.html?id={}'.format(id)
+
+
+    reward = competition['reward']
+
+    return name, url, reward
+
+
 def get_data():
-    url = 'https://challenge.datacastle.cn/v3/api/common/cmpt/getCmptListES.json?keword=&orderType=&type=%E7%AE%97%E6%B3%95%E8%B5%9B&state=1&page=1&pageSize=10'
+    url = 'https://challenge.datacastle.cn/v3/api/common/cmpt/getCmptListES.json?keword=&orderType=&type=&state=&page=1&pageSize=10'
 
     response = requests.get(url=url)
     content = response.json()
@@ -15,31 +29,39 @@ def get_data():
     data = {'name': PLATFORM_NAME}
     cps = []
     for competition in competitions:
-        if competition['rewardType'] != '奖金':
-            continue
-        # 必须字段
-        name = competition['cmptName']
-        id = competition['id']
-        url = 'https://challenge.datacastle.cn/v3/cmptDetail.html?id={}'.format(
-            id)
         description = competition['introduction']
-
         FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
         deadline = datetime.strptime(competition['endTime'], FORMAT)
         start_time = datetime.strptime(competition['startTime'], FORMAT)
+        
+        if competition['ztsList']:
+            for c in competition['ztsList']:
+                name, url, reward = parse_competition(
+                    c)
+                cp = {
+                    'name': name,
+                    'url': url,
+                    'description': description,
+                    'deadline': deadline,
+                    'reward': reward,
+                    'start_time': start_time,
+                }
 
-        reward = competition['reward']
+                cps.append(cp)
+        else:
+            name, url, reward = parse_competition(
+                competition)
+            cp = {
+                'name': name,
+                'url': url,
+                'description': description,
+                'deadline': deadline,
+                'reward': reward,
+                'start_time': start_time,
+            }
 
-        cp = {
-            'name': name,
-            'url': url,
-            'description': description,
-            'deadline': deadline,
-            'reward': reward,
-            'start_time': start_time,
-        }
+            cps.append(cp)
 
-        cps.append(cp)
     data['competitions'] = cps
 
     return data
