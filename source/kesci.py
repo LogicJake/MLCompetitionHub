fa3a3e7235a9efc817d68b32d976f45da65691e8
scy_labtop <835410808@qq.com>
2022-01-31 14:44:50 +0800

    update kesci

--- source/kesci.py
+++ source/kesci.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from termios import CINTR
 
 import requests
 
@@ -6,7 +7,7 @@ PLATFORM_NAME = 'heywhale和鲸（Kesci）'
 
 
 def get_data():
-    url = 'https://www.kesci.com/v2/api/competitions?perPage=12&page=1&Status=1'
+    url = 'https://www.heywhale.com/v2/api/competitionsCommunity?perPage=12&page=1&excludeDetailType=WORKSHOP,TRAINING_CAMP,EXERCISE_DATA_ANALYSIS,EXERCISE_ALGORITHM,OTHER&Status=1'
 
     response = requests.get(url=url)
     content = response.json()
@@ -15,37 +16,82 @@ def get_data():
     data = {'name': PLATFORM_NAME}
     cps = []
     for competition in competitions:
-        if competition['DisplayLabel'] == '练习赛' or competition[
-                'DisplayLabel'] == '训练营' or competition[
-                    'DisplayLabel'] == 'DATA TRAIN':
-            continue
-        # 必须字段
-        name = competition['Name']
-        url = 'https://www.kesci.com/home/competition/' + competition['_id']
-        description = competition['ShortDescription']
-
-        deadline = competition['EndDate']
         FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
-        if deadline is not None:
-            deadline = datetime.strptime(deadline, FORMAT) + timedelta(hours=8)
+
+        if 'IsSeriesCompetition' in competition:
+            childrenCompetitions = competition['childrenCompetitions']
+            parent_name = competition['Name']
+
+            for competition in childrenCompetitions:
+                if competition['DetailType'] != 'ALGORITHM':
+                    continue
+
+                name = parent_name + '——' + competition['Name']
+                url = 'https://www.kesci.com/home/competition/' + competition[
+                    '_id']
+                description = name
+
+                deadline = competition['EndDate']
+                if deadline is not None:
+                    deadline = datetime.strptime(deadline,
+                                                 FORMAT) + timedelta(hours=8)
+                else:
+                    deadline = None
+
+                start_time = competition['StartDate']
+                start_time = datetime.strptime(start_time,
+                                               FORMAT) + timedelta(hours=8)
+
+                if competition['DisplayLabel'] == '':
+                    reward = competition['Award']
+                else:
+                    reward = competition['DisplayLabel']
+
+                cp = {
+                    'name': name,
+                    'url': url,
+                    'description': description,
+                    'deadline': deadline,
+                    'reward': reward,
+                    'start_time': start_time,
+                }
+
+                cps.append(cp)
         else:
-            deadline = None
+            if competition['DetailType'] != 'ALGORITHM':
+                continue
 
-        start_time = competition['StartDate']
-        start_time = datetime.strptime(start_time, FORMAT) + timedelta(hours=8)
+            # 必须字段
+            name = competition['Name']
+            url = 'https://www.kesci.com/home/competition/' + competition['_id']
+            description = name
 
-        reward = competition['DisplayLabel']
+            deadline = competition['EndDate']
+            if deadline is not None:
+                deadline = datetime.strptime(deadline,
+                                             FORMAT) + timedelta(hours=8)
+            else:
+                deadline = None
 
-        cp = {
-            'name': name,
-            'url': url,
-            'description': description,
-            'deadline': deadline,
-            'reward': reward,
-            'start_time': start_time,
-        }
+            start_time = competition['StartDate']
+            start_time = datetime.strptime(start_time,
+                                           FORMAT) + timedelta(hours=8)
 
-        cps.append(cp)
+            if competition['DisplayLabel'] == '':
+                reward = competition['Award']
+            else:
+                reward = competition['DisplayLabel']
+
+            cp = {
+                'name': name,
+                'url': url,
+                'description': description,
+                'deadline': deadline,
+                'reward': reward,
+                'start_time': start_time,
+            }
+
+            cps.append(cp)
     data['competitions'] = cps
 
     return data
